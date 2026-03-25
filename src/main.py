0f99cfea3a550b11d45f9a04f0822d45d32e6fda
Manish Dhakal <dhakalmanish398@gmail.com>
2026-03-25 09:21:24 +0545

    Build training structure and initialize the training loop

---
+++ src/main.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+from tokenize import Encoder
+from models import CNN, LSTM
+from loader import load_dataset
+
+from train import train_models
+
+
+
+if __name__ == "__main__":
+    
+
+    #Define hyperparams
+    lr = 0.01
+    epochs = 15
+
+    device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
+
+
+   
+
+    tokenizer = Encoder()
+
+    print("Loading dataloaders......")
+    train_loader, test_loader = load_dataset(r"D:/Python/ImgCaptionGenerator/data/images",
+                                              r"D:/Python/ImgCaptionGenerator/data/captions.txt",
+                                                tokenizer)
+    
+    print("Dataloaders loaded successfully..../n")
+
+    
+    vocab_size = tokenizer.max_length
+    embed_dim = 256
+
+    print("Initializing CNN and LSTM....")
+
+    cnn_model = CNN(embed_dim, device)
+    lstm_model = LSTM(embed_dim, device, vocab_size=tokenizer.max_length)
+
+    print("CNN and LSTM Initialized successfully")
+
+
+    print("Starting Training.....")
+
+    train_models(CNN, LSTM, train_loader, epochs = epochs ,learning_rate=lr, pad_index= tokenizer.pad_index)
+
+
+
+
+
--- src/train.py
+++ src/train.py
@@ -1,2 +1,39 @@
 from models import CNN, LSTM
+import torch
+import torch.nn as nn
+
 from loader import load_dataset
+
+
+def train_models(CNN, LSTM, train_loader, epochs, learning_rate, pad_index):
+
+
+    optimizer = torch.optim.SGD(params = LSTM.parameters(), lr= learning_rate, momentum=0.08, weight_decay= 1e-4)
+
+    loss_fn = nn.CrossEntropyLoss(ignore_index=pad_index)
+
+    for epoch in range(epochs):
+
+        running_loss = 0.0
+
+        LSTM.train()
+        
+        for batch in train_loader:
+
+            images, input_captions, target_captions  = batch[0], batch[1], batch[2]
+
+            img_features = CNN(images)
+
+            y_pred = LSTM(images,input_captions)
+
+            loss = loss_fn(y_pred, target_captions)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss
+        
+        print(f"Epoch {epoch} Running Loss : {running_loss}")
+            
+
